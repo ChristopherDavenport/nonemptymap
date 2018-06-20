3a877c715f98b0660c902930e43bc5cd7d9a2022
Chris Davenport <chris@christopherdavenport.tech>
2018-06-20 14:14:10 -0400

    Fix Version Bounds

--- src/Data/Map/NonEmpty.hs
+++ src/Data/Map/NonEmpty.hs
@@ -1,5 +1,4 @@
 {-# language InstanceSigs #-}
-
 module Data.Map.NonEmpty(
   NonEmptyMap(..), -- Generic Constructor
   singleton -- :: (k, v) -> NonEmptyMap k v
@@ -13,11 +12,18 @@ data NonEmptyMap k v = NonEmptyMap (k, v) (Map.Map k v)
 
 
 instance Functor (NonEmptyMap k) where
-  fmap :: (v -> b) -> NonEmptyMap k v -> NonEmptyMap k b
+  fmap :: (a -> b) -> NonEmptyMap k a -> NonEmptyMap k b
   fmap f (NonEmptyMap (k, v) map) =  NonEmptyMap (k, f v) (fmap f map)
 
+-- Construction
 singleton :: (k, v) -> NonEmptyMap k v
 singleton tup = NonEmptyMap tup Map.empty
 
 
 
+
+-- , insert
+
+-- , insertWith
+-- , insertWithKey
+-- , insertLookupWithKey
